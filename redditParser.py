0d80d6fb7aee3c72281e359f16f9abb279ea5145
abeshry <abeshry@gmail.com>
2015-06-08 16:25:45 -0700

    Reddit parser retrieves all item information
    For the first 2 pages, all item information and links are retrieved
    successfully

--- redditParser.py
+++ redditParser.py
@@ -11,27 +11,48 @@ class redditParser(Downloader):
 	def __init__(self):
 		self.url = "http://reddit.com/r/all"
 		self.today = ""
+		self.lastID = ""
 
 	def getTitles(self):
 		self.download()
+		itemArray = []
 		if (self.contents):
+			#count of titles wanted
 			count = 0
-			while (count < 501):
+			while (count < 50):
 				i = 1
 				self.download()
+				time.sleep(0.1)
 				while (i < 51):
+					item = redditItem()
 					tree = etree.HTML(self.contents)
-					redditurl = tree.xpath("string(//*[@id='siteTable']/div["+str(i)+"]/div[2]/p[1]/a)")
-					print(redditurl)
+					item.rank = int((i + 1)/2)
+					item.title = tree.xpath("string(//*[@id='siteTable']/div["+str(i)+"]/div[2]/p[1]/a)")
+					item.link = tree.xpath("string(//*[@id='siteTable']/div["+str(i)+"]/div[2]/p[1]/a/@href)")
+					item.date = tree.xpath("string(//*[@id='siteTable']/div["+str(i)+"]/div[2]/p[2]/time/@title)")
+					item.user = tree.xpath("string(//*[@id='siteTable']/div["+str(i)+"]/div[2]/p[2]/a[1])")
+					item.thumbnail = tree.xpath("string(//*[@id='siteTable']/div["+str(i)+"]/a/img/@src)") #CREATE OWN
+					if (item.thumbnail != ""):
+						item.hasThumbnail = True
 					i = i + 2
-				pageLast = tree.xpath("string(//*[@id='siteTable']/div[49]/@class)")
-				pageLast = pageLast[10:]
-				print(pageLast)
-				pageLast = pageLast[:9]
-				print(pageLast)
+					itemArray.append(item)
+					print(item.user)
+
+				pageLastID = tree.xpath("string(//*[@id='siteTable']/div[49]/@data-fullname)")
+
 				count = count + 25
-				self.url = "http://www.reddit.com/r/all/?count="+str(count) + "&after=" + str(pageLast)
-				print(self.url)
+				self.url = "http://www.reddit.com/r/all/?count="+str(count) + "&after=" + str(pageLastID)
+
+class redditItem():
+	def __init__(self):
+		self.title = ""
+		self.link = ""
+		self.user = ""
+		self.date = ""
+		self.rank = ""
+		self.hasThumbnail = False
+		self.thumbnail = ""
+
 
 if __name__ == "__main__":
 	if (not os.path.exists ("Reddit")):
